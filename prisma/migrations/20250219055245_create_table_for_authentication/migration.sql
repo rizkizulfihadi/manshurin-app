8f4c5c74e31664423bc34edd0c4d68002643e839
Rizki Zulfihadi <rizkizulfihadi17@gmail.com>
2025-02-19 13:33:42 +0700

    chore : initialize prisma and setup table for authentication

---
+++ prisma/migrations/20250219055245_create_table_for_authentication/migration.sql
@@ -0,0 +1,80 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('male', 'female');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('user', 'admin', 'root');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "picture" TEXT NOT NULL,
+    "provider" TEXT NOT NULL DEFAULT 'credentials',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "otpVerified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "otp" (
+    "id" TEXT NOT NULL,
+    "code" VARCHAR(6) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "personal_infos" (
+    "id" TEXT NOT NULL,
+    "fullName" VARCHAR(50) NOT NULL,
+    "gender" "Gender",
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "personal_infos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "password_reset_requests" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "requestCount" INTEGER NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "password_reset_requests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "personal_infos_userId_key" ON "personal_infos"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password_reset_requests_email_key" ON "password_reset_requests"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password_reset_requests_token_key" ON "password_reset_requests"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password_reset_requests_userId_key" ON "password_reset_requests"("userId");
+
+-- AddForeignKey
+ALTER TABLE "otp" ADD CONSTRAINT "otp_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "personal_infos" ADD CONSTRAINT "personal_infos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "password_reset_requests" ADD CONSTRAINT "password_reset_requests_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
